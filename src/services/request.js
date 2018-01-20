ea77e99f79023efd556f0058917387e687b7eb6e
marcos.mezzabotta <marcos.mezzabotta@intive-fdv.com>
2018-01-20 01:45:29 -0300

    Add sweetAlert to catch errors in the service

--- src/services/request.js
+++ src/services/request.js
@@ -4,5 +4,5 @@ const appId = '68fbc6ebe3634d78bd9c8502ac9d39fc';
 
 export default {
   latest: () => axios.get(`https://openexchangerates.org/api/latest.json?app_id=${appId}`),
-  currecies: () => axios.get('https://openexchangerates.org/api/currencies.json'),
+  currecies: () => axios.get('https://openexchangerates.org/api/currencies.json'), // deprecated
 };
--- src/utils/sagasHelpers.js
+++ src/utils/sagasHelpers.js
@@ -1,4 +1,6 @@
 import { call, put } from 'redux-saga/effects';
+import swal from 'sweetalert';
+
 
 export function* performRequest(service, requestActionCreator) {
   yield put(requestActionCreator.pending);
@@ -6,6 +8,7 @@ export function* performRequest(service, requestActionCreator) {
     const response = yield call(service);
     yield put(requestActionCreator.success(response));
   } catch (error) {
+    swal ( "Oops" ,  "Something went wrong! Check your connection and try again. ",  "error" )
     yield put(requestActionCreator.error(error));
   }
 }
